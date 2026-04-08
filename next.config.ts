7ddfc6434dcab7b930842e00deca6d876953855e
Douglas DRM <douglasdrm@gmail.com>
2026-04-08 10:50:57 -0300

    fix: added serverExternalPackages for prisma, pg and bcryptjs in next.config

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  serverExternalPackages: ["@prisma/client", "prisma", "pg", "bcryptjs"],
 };
 
 export default nextConfig;
