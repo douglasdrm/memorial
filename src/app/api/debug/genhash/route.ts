54e62d612c62f011d1d5eab71e90b7b737e9efeb
Douglas DRM <douglasdrm@gmail.com>
2026-04-08 12:01:19 -0300

    security: remove debug endpoints from production

--- src/app/api/debug/genhash/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import bcrypt from "bcryptjs";
-
-export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
-  const password = searchParams.get("p") || "memorial123";
-  const hash = await bcrypt.hash(password, 10);
-  return NextResponse.json({ password, hash });
-}
